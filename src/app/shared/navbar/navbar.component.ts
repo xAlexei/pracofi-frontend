58f9407e9212b876373cde38b2945952bedb75d3
xAlexei <sukesasuk2010@gmail.com>
2022-05-19 11:02:49 -0500

    [ADD] Dialog login

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd, NavigationStart } from '@angular/router';
 import { Location, PopStateEvent } from '@angular/common';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDate, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-navbar',
@@ -8,11 +10,79 @@ import { Location, PopStateEvent } from '@angular/common';
     styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  fromDate: NgbDate;
+  toDate: NgbDate;
+  hoveredDate: NgbDate;
+  closeResult: string;
+  model1 : NgbDate;
+  model2 : NgbDate;
+  model : NgbDate;
+
+  focus;
+  focus1;
+  focus2;
+  focus3;
+  focus4;
+
+
     public isCollapsed = true;
     private lastPoppedUrl: string;
     private yScrollStack: number[] = [];
 
-    constructor(public location: Location, private router: Router) {
+    constructor(private modalService: NgbModal, calendar: NgbCalendar, public location: Location, private router: Router) {
+    }
+    
+    open(content, type, modalDimension) {
+        if (modalDimension === 'sm' && type === 'modal_mini') {
+            this.modalService.open(content, { windowClass: 'modal-mini', size: 'sm', centered: true }).result.then((result) => {
+                this.closeResult = `Closed with: ${result}`;
+            }, (reason) => {
+                this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+            });
+        } else if (modalDimension === '' && type === 'Notification') {
+          this.modalService.open(content, { windowClass: 'modal-danger', centered: true }).result.then((result) => {
+              this.closeResult = `Closed with: ${result}`;
+          }, (reason) => {
+              this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+          });
+        } else {
+            this.modalService.open(content,{ centered: true }).result.then((result) => {
+                this.closeResult = `Closed with: ${result}`;
+            }, (reason) => {
+                this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+            });
+        }
+    }
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return  `with: ${reason}`;
+        }
+    }
+    isRangeStart(date: NgbDate){
+      return this.model1 && this.model2 && date.equals(this.model1);
+    }
+    isRangeEnd(date: NgbDate){
+      return this.model1 && this.model2 && date.equals(this.model2);
+    }
+    isInRange(date: NgbDate){
+      return date.after(this.model1) && date.before(this.model2);
+    }
+    isActive(date: NgbDate){
+      return date.equals(this.model1) || date.equals(this.model2);
+    }
+    endDateChanged(date){
+      if (this.model1 && this.model2 && (this.model1.year > this.model2.year || this.model1.year === this.model2.year && this.model1.month > this.model2.month || this.model1.year === this.model2.year && this.model1.month === this.model2.month && this.model1.day > this.model2.day )) {
+        this.model1 = this.model2;
+      }
+    }
+    startDateChanged(date){
+      if (this.model1 && this.model2 && (this.model1.year > this.model2.year || this.model1.year === this.model2.year && this.model1.month > this.model2.month || this.model1.year === this.model2.year && this.model1.month === this.model2.month && this.model1.day > this.model2.day )) {
+        this.model2 = this.model1;
+      }
     }
 
     ngOnInit() {
